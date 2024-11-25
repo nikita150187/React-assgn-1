cc0d08cdb8d8e4c496f3c023749784beef652551
nikita150187 <nikita.taware@stud.sundsgarden.se>
2024-11-25 14:23:34 +0530

    create react gallary

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Product1, Product2, Product3, Product4, Product5, } from "./componants/gallery.jsx";
+
+function App(){
+  return(
+    <>
+       <Product1 productName1="Women Painting" productPrice="800"/>
+       <Product2 productName2="Acrylic Painting" productPrice="500"/>
+       <Product3 productName3="canvas Painting" productPrice="650"/>
+       <Product4 productName4="Fall Painting" productPrice="600"/>
+       <Product5 productName5="Snowy Lakeside" productPrice="700"/>
+       
+    </>
+   );
+  }
+  export default App;
